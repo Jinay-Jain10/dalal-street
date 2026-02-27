34c6571d60a3311526f5bd853ad4fdd124db5080
Jinay-Jain10 <jinay.jain1234@gmail.com>
2026-02-27 23:23:23 +0530

    Change Url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://dalal-street.vercel.app/',
+    'https://dalal-street.vercel.app',
   ],
 }));
 app.use(express.json());
