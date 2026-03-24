e5a7843edbe052360ae11018377ee2abe407d151
Jinay-Jain10 <jinay.jain1234@gmail.com>
2026-03-25 00:54:02 +0530

    install express rate limit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
+const rateLimit = require('express-rate-limit');
 require('./models/index');
 
 const authRoutes = require('./routes/auth');
